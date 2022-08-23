ff135cb7e2949245dafe23747e0773d58f9c250d
dolyu <dolyu@wgslabs.com>
2022-08-23 18:56:50 +0900

    [22-08-23] cuda 최적화

--- HighPassFilter.h
+++ HighPassFilter.h
@@ -27,22 +27,6 @@ typedef unsigned long ULONG;
 typedef unsigned int UINT;
 
 #define UNIT_COUNT 200
+#define BEFORE_DATA_COUNT 100
 
-// const float HF_ST1 = 0.125;
-// const float HF_CF1 = 0.25;
-// const float HF_ST2 = 0.125;
-// const float HF_CF2 = 10;
-// const double IDT = HF_ST1;
-// const double OMEGA_C_1 = 2 * M_PI * HF_CF1;
-// const double OMEGA_C_2 = 2 * M_PI * HF_CF2;
-// const double AMPLFAC_1 = 1 / ((IDT * OMEGA_C_1 / 2) + 1);
-// const double AMPLFAC_2 = 1 / ((IDT * OMEGA_C_2 / 2) + 1);
-// const double Y1C_1 = (IDT * OMEGA_C_1 / 2) - 1;
-// const double Y1C_2 = (IDT * OMEGA_C_2 / 2) - 1;
-// const double DT = IDT;
-
-// EXPORT int cudaHighPassFilter(const uint8_t* src, const int cnt, uint8_t* dest, uint8_t* max);
-// EXPORT int cudaHighPassFilter(const uint8_t* src, const int cnt, uint8_t* dest_1, uint8_t* dest_2, uint8_t* filter_1, uint8_t* filter_2, float* max_1, float* max_2);
-// EXPORT int cudaHighPassFilter(const uint8_t* src, const int cnt, uint8_t* dest_1, uint8_t* dest_2, float* max_1, float* max_2);
-// EXPORT int cudaHighPassFilter(const uint8_t* src, const int cnt, float* max_1, float* max_2,float* filter_1, float* filter_2, float hf_st1, float hf_cf1, float hf_st2, float hf_cf2);
-EXPORT int cudaHighPassFilter(const uint8_t* src_1,const uint8_t* src_2, const int cnt, float* max_1, float* max_2,float* filter_1, float* filter_2, float hf_st1, float hf_cf1, float hf_st2, float hf_cf2);
+EXPORT int cudaHighPassFilter(const uint8_t * before_data_1, const uint8_t * before_data_2,const uint8_t* src, const int cnt, float* max_1, float* max_2, double hf_st1, double hf_cf1, double hf_st2, double hf_cf2);
